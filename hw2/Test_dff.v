7c1c4d946bab1ce51a8ddf237e582754663c6651
bezhkodi <96302837+bezhkodi@users.noreply.github.com>
2021-12-21 14:28:33 +0300

    Update Test_dff.v

--- hw2/Test_dff.v
+++ hw2/Test_dff.v
@@ -1,4 +1,4 @@
-module TestDFF;
+module test_dff;
 	reg clock = 0;
 	reg d = 0;
 	wire q;
@@ -6,7 +6,7 @@ module TestDFF;
 	always #10 clock <= ~clock;
 	d_flip_flop d_flip_flop(.d(d), .e(clock), .q(q), .q_n(q_n));
 	initial begin
-		$dumpvars(0, TestDFF);
+		$dumpvars(0, test_dff);
 		d = 0;
 		#10; d = 1;
 		#10; d = 0;
