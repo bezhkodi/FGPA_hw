cec80ed10c8e9a96da5142a3f7c8027dfb458f71
bezhkodi <96302837+bezhkodi@users.noreply.github.com>
2021-12-21 15:00:28 +0300

    Update sr_latch.v

--- hw2/sr_latch.v
+++ hw2/sr_latch.v
@@ -1,8 +1,8 @@
 module sr_latch(
-    input s,
-    input r,
-    output q,
-    output q_n
+    input wire s,
+    input wire r,
+    output wire q,
+    output wire q_n
 );
   assign q   =~ ( r | q_n);
   assign q_n =~ ( s | q );
